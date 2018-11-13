00a5ffcf1feb8ee86a82ff11363f516a75231f4c
Diego Krefta <diego.krefta@gmail.com>
2018-11-13 12:43:11 -0200

    Resolve function

--- schema/schema.js
+++ schema/schema.js
@@ -1,16 +1,30 @@
-const graphql = require('graphql')
+const graphql = require("graphql");
+const _ = require("lodash");
 
-const {
- GraphQLObjectType,
- GraphQLString,
- GraphQLInt
-} = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt } = graphql;
 
+const user = [
+  { id: "23", firstName: "Bill", age: "20" },
+  { id: "47", firstName: "Samantha", age: "21" }
+];
 const UserType = new GraphQLObjectType({
-  name: 'User',
+  name: "User",
   fields: {
-    id: {type: GraphQLString},
-    firstName: {type: GraphQLString},
-    age: {type:GraphQLInt}
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt }
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return _.find(users, { id: args.id });
+      }
+    }
   }
 });
